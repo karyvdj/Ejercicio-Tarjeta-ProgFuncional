712265f43b0fad7a3117d071670a5ad58f13ab3f
Kary V Dj <KaryVDj@iMac-de-Kary.local>
2017-04-09 21:53:21 -0500

    En proceso de funcion validacion de cifras

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -11,6 +11,11 @@ function isValidCard(creditCardNumber) {
     if (cantidadNumeros(creditCardNumber) != true){
       return("Faltan numeros");
     }
+    if(validarCifra(creditCardNumber) == true){
+      return (true);
+    } else{
+      return (false);
+    }
 }
 
 function cantidadNumeros(creditCardNumber) {
@@ -19,6 +24,27 @@ function cantidadNumeros(creditCardNumber) {
     return(true);
   }
 }
+
+function validarCifra(numeros){
+  var cifra = numeros.toString();
+  cifra = cifra.split("");
+  var cifraString = (numeros.toString()).split("");
+
+  var numero = cifraString.map(conversionNum);
+  var pares = cifra.filter(pares);
+}
+
+function conversionNum(numero){
+  return parseInt(numero);
+}
+
+function pares(numero,i){
+  return i % 2 == 0;
+}
+function impares(numero,i){
+  return i % 2 != 0;
+}
+
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
     exports.isValidCard = isValidCard;
